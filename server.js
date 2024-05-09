f26653f9456d8848b616d9fc3dff027229ff06e2
Ben Woodring <ben@benjaminjwoodring.com>
2024-05-09 13:51:32 +0000

    bug fixes

--- server.js
+++ server.js
@@ -16,14 +16,17 @@ const quoteSchema = new mongoose.Schema({
 	body: String
 });
 
-//const Quote = mongoose.model('Quote', quoteSchema, 'thirtynine');
+const confessionSchema = new mongoose.Schema({
+    prettyName: String
+}, { collection: 'confessions' }); 
+const Confession = mongoose.model('Confession', confessionSchema);
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/reformed', (req, res) => {
+app.get('/cccapi', (req, res) => {
 	const search = req.query.search;
-	const colName = req.query.col;
+	const colName = req.query.doc;
 	const Quote = mongoose.model('Quote', quoteSchema, colName);	
 	if (search) {
 		Quote.find({ body: { $regex: search, $options: 'i' } })
@@ -35,7 +38,28 @@ app.get('/reformed', (req, res) => {
 			.catch(err => res.status(400).json('Error: ' + err));
 	}
 });
+
+app.get('/cccapi/getcollections', async (req, res) => {
+	try {
+        const prettyNames = await Confession.find({}, 'name prettyname -_id'); // Select only the prettyName field, exclude _id
+        res.json(prettyNames);
+    } catch (error) {
+        console.error("Failed to retrieve collections:", error);
+        res.status(500).send("Failed to retrieve collections");
+    }
+});
+
+app.get('/cccapi/getcollections', async (req, res) => {
+	try {
+        const prettyNames = await Confession.find({}, 'prettyname -_id'); // Select only the prettyName field, exclude _id
+        res.json(prettyNames);
+    } catch (error) {
+        console.error("Failed to retrieve collections:", error);
+        res.status(500).send("Failed to retrieve collections");
+    }
+});
+
 app.listen(PORT, () => {
-	console.log(`ReformedAPI is running on port 1611.`);
+	console.log(`CCCAPI is running on port 1611.`);
 });
 
